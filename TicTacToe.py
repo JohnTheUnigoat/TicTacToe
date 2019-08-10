3420b2cc4eefac4422a0ed2c7b63965b844669f3
= <=>
2019-08-10 13:36:58 +0300

    Added field drawing

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,3 +9,22 @@ field = [
     [0, 0, 0],
     [0, 0, 0]
 ]
+
+
+def print_field():
+    for i in range(3):
+        print('     |     |')
+        print(
+            '  ' + cell_states[field[i][0]] + '  |' +
+            '  ' + cell_states[field[i][1]] + '  |' +
+            '  ' + cell_states[field[i][2]]
+        )
+        if i == 2:
+            print('     |     |')
+        else:
+            print('_____|_____|_____')
+
+
+print_field()
+
+input()
